c02e2b5025824b7d4d996bf92cf38930d2f591a0
greenmangquery@gmail.com <greenmangquery@gmail.com>
2020-09-14 14:28:02 +0800

    enable swagger

--- quarkus-jwt/src/main/java/org/code/controller/ExampleResource.java
+++ quarkus-jwt/src/main/java/org/code/controller/ExampleResource.java
@@ -1,6 +1,16 @@
 package org.code.controller;
 
 import org.code.model.User;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.tags.Tags;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -14,9 +24,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Path("/v1")
+@SecurityScheme(securitySchemeName = "jwt", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "jwt")
 public class ExampleResource {
     @GET
     @Path("/testing")
+    @Tags(value = @Tag( name="Token", description = "Its token testing path"))
+    @SecurityRequirement(name="jwt", scopes = {})
+    @Operation(summary = "this just testing url with token")
+    @APIResponses(value = {@APIResponse(responseCode = "403", description = "Token not valid"),
+    @APIResponse(responseCode = "200", description = "return data", content = @Content(schema = @Schema(implementation = Map.class)))})
     @Produces(MediaType.APPLICATION_JSON)
     public Response test(@Context SecurityContext context){
         Map<String, String> test= new HashMap<>();
